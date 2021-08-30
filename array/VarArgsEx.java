e67d57aa51a41e9aa92137c9f960bbd469067d69
This2sho <zx00018@naver.com>
2021-08-30 20:22:30 +0900

    "edit" 예제 주석 못봄

--- array/VarArgsEx.java
+++ array/VarArgsEx.java
@@ -5,8 +5,10 @@ public class VarArgsEx {
         String[] strArr = {"100", "200", "300"};
 
         System.out.println(concatenate("", "100", "200", "300"));
-
-
+        System.out.println(concatenate("-", strArr));
+        System.out.println(concatenate(",", new String[]{"1", "2", "3"}));
+        System.out.println("["+concatenate(",", new String[0]) +"]");
+        System.out.println("[" + concatenate(",")+ "]");
 
     }   
     
