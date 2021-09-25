ff1e07fff0c43e81cff9dcf2182ee70c6c6e61ec
This2sho <zx00018@naver.com>
2021-09-25 23:47:40 +0900

    add "Thread practice 2"

---
+++ Thread/ThreadEx7.java
@@ -0,0 +1,26 @@
+package Thread;
+
+import javax.swing.JOptionPane;
+
+class ThreadEx7 {
+    public static void main(String[] args) throws Exception{
+
+        ThreadEx7_1 th1 = new ThreadEx7_1();
+        th1.start();
+
+        String input = JOptionPane.showInputDialog("아무값이나 입력하세요.");
+        System.out.println("입력하신 값은 " + input + "입니다.");
+
+    }
+}
+
+class ThreadEx7_1 extends Thread {
+    public void run(){
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+            try{
+                sleep(100);
+            } catch(Exception e){}
+        }
+    }
+}
